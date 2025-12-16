72d06d32b2e6d695f0a11b02adc4f314695634fc
Minhyeok <112698340+hanraeul@users.noreply.github.com>
2025-12-16 21:28:34 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        for(int i = 0 ; i < t ; i++) {
+            
+            if(answer == 0) {
+                answer = n * 2;
+            }
+            else {
+                answer = answer * 2;
+            }
+        }
+        
+        return answer;
+    }
+}
